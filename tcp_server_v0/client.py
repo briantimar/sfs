955735ac718cf471d91de1ed40a70f65c6a7d5c9
Brian Timar <timarbrian@gmail.com>
2020-02-26 19:47:35 -0800

    test server python

---
+++ tcp_server_v0/client.py
@@ -0,0 +1,28 @@
+import socket
+
+class Client:
+
+    def __init__(self, port_number=None, send_buf_size=None, rec_buf_size=None):
+        self.port_number = port_number
+        self.send_buf_size = send_buf_size
+        self.rec_buf_size = rec_buf_size
+
+    def log(self, s):
+        print(s)
+
+    def send(self, foreign_addr, foreign_port, message):
+        """Send a bytes message to TCP server at address and port specified."""
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            self.log(f"Connecting to {foreign_addr}, port {foreign_port}...")
+            if self.port_number is not None:
+                s.bind(('', self.port_number))
+            s.connect((foreign_addr, foreign_port))
+            self.log(f"Using local socket {s.getsockname()}")
+            nb = s.send(message)
+            self.log(f"{nb} bytes sent.")
+        self.log("Done.")
+        
+
+if __name__ == "__main__":
+    cl = Client(port_number=61877)
+    cl.send('127.0.0.1', 56500, b'hello')
---
+++ tcp_server_v0/server.py
@@ -0,0 +1,44 @@
+""" A toy server using python's sockets module - just to demonstrate how to use the TCP api"""
+import logging
+import socket
+
+class Server:
+
+    def __init__(self, port_number, rec_buf_size, send_buf_size):
+        """port_number: int. port number to bind to.
+            rec_buf_size: size of the receive buffer in bytes
+            send_buf_size: size of the send buffer."""
+
+        self.port_number = port_number
+        self.rec_buf_size = rec_buf_size
+        self.send_buf_size = send_buf_size
+
+    def log(self, s):
+        logging.info(s)
+
+    def start(self):
+        """Starts running the server: OPEN the local port, with no foreign socket specified.
+        """
+        self.log(f"Starting server listening at port {self.port_number}...")
+        # create an IP socket
+        with socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            # set the port number
+            s.bind(('', self.port_number))
+            #allows the socket to accept connections
+            s.listen(1)
+            while True:
+                # blocks until a connection is received
+                connection, address = s.accept()
+                self.log(f"Accepted a connection from {address}")
+                # now, receive data from the connection
+                data = connection.recv(self.rec_buf_size)
+                self.log(f"received data: {repr(data)}")
+                
+
+
+
+if __name__ == "__main__":
+    server = Server(56500, 1024, 2)
+    logging.basicConfig(level=logging.INFO)
+    server.start()
