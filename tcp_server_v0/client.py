31ff510691e14747fbe58e654c74f696b9a89cd8
Brian Timar <timarbrian@gmail.com>
2020-02-26 21:37:57 -0800

    move client to TCPModule

--- tcp_server_v0/client.py
+++ tcp_server_v0/client.py
@@ -1,27 +1,38 @@
+import argparse
 import socket
+import sys
 
-class Client:
+from server import TCPModule
+
+class Client(TCPModule):
 
     def __init__(self, port_number=None, rec_buf_size=1024):
-        self.port_number = port_number
-        self.rec_buf_size = rec_buf_size
+        super().__init__(port_number=port_number, rec_buf_size=rec_buf_size)
+        if self.port_number is not None:
+            self.port_number = int(self.port_number)
+            self.log(f"Starting client with static port number {self.port_number}...")
 
-    def log(self, s):
-        print(s)
 
     def send(self, foreign_addr, foreign_port, message):
         """Send a bytes message to TCP server at address and port specified."""
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             self.log(f"Connecting to {foreign_addr}, port {foreign_port}...")
+
             if self.port_number is not None:
-                s.bind(('', self.port_number))
+                self.log(f"Binding to local port {self.port_number}...")
+                s.bind(('127.0.0.1', self.port_number))
             s.connect((foreign_addr, foreign_port))
             self.log(f"Using local socket {s.getsockname()}")
-            nb = s.send(message)
-            self.log(f"{nb} bytes sent.")
+
+            s.sendall(message)
+            s.shutdown(socket.SHUT_RDWR)
         self.log("Done.")
         
+    
 
 if __name__ == "__main__":
-    cl = Client(port_number=61877)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", default=None)
+    args = parser.parse_args()
+    cl = Client(port_number=args.port)
     cl.send('127.0.0.1', 56500, b'hello')
