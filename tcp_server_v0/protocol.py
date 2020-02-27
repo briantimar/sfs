ba257e9d2bf70773311b7c37b40ec991acff2b54
Brian Timar <timarbrian@gmail.com>
2020-02-26 23:37:13 -0800

    len-prefix protocol

--- tcp_server_v0/protocol.py
+++ tcp_server_v0/protocol.py
@@ -2,6 +2,7 @@
 from math import log10, ceil
 #max length of any single message, in bytes
 MAX_LENGTH_LOG10 = 9
+PREFIX_SIZE = MAX_LENGTH_LOG10 + 1
 
 def encode_length(n):
     """Encodes integer length into fixed-size  string."""
@@ -13,6 +14,14 @@ def encode_length(n):
         raise ValueError(f"Message size of {n} bytes is too large.")
     return "0" * (MAX_LENGTH_LOG10 - nd) + str(n)
 
+def decode_length(bytes_prefix):
+    """Returns integer corresponding to the given bytes prefix, eg 0005: -> 5"""
+    assert len(bytes_prefix) == PREFIX_SIZE
+    return int(bytes_prefix[:-1])
+
 def len_encode(message):
     """Encodes a bytes message, by prepending the length of the message."""
-    return encode_length(len(message)) + ":" + message
+    return bytes(encode_length(len(message)), "ascii") + b":" + message
+
+def encode(message):
+    return len_encode(message)
