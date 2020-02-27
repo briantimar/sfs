eebad2fc1dd1e0a6d498ef1777215e9039faa2c9
Brian Timar <timarbrian@gmail.com>
2020-02-26 23:36:44 -0800

    bytesarray read buffer

--- tcp_server_v0/server.py
+++ tcp_server_v0/server.py
@@ -1,6 +1,8 @@
 """ A toy server using python's sockets module - just to demonstrate how to use the TCP api"""
 import logging
 import socket
+import argparse
+from protocol import PREFIX_SIZE, decode_length, encode
 
 class TCPModule:
     """One half of a TCP connection."""
@@ -11,27 +13,48 @@ class TCPModule:
         self.port_number = port_number
         self.rec_buf_size = rec_buf_size
 
-        #read individual recv calls into here
-        self._read_buffer = []
-        # the result of a 'full read' operation
-        self._read = b''
+        self._id = "" if self.port_number is None else str(self.port_number)
+
+        #local read buffer, currently with no size limit
+        self._read_buffer= bytearray(b'')
+        # current bytes chunk
+        self._chunk = b''
 
     def log(self, s):
         print(s)
 
-    def flush_read(self):
+    def _flush_read(self):
         """Flush the read buffer."""
-        self._read = b''.join(self._read_buffer)
-        self._read_buffer = []
+        self._read_buffer = bytearray(b'')
 
-    def _read_all(self, connection):
-        """Read bytes from the given connection into the read buffer, until no more remain."""
-        data = connection.recv(self.rec_buf_size)
-        while data:
-            self._read_buffer.append(data)
-            data = connection.recv(self.rec_buf_size)
-        self.flush_read()
-        self.log(f"Received data: {self._read}")
+
+    def _fill_buffer(self, connection, n):
+        """Fill the read buffer from the given connection, until it contains at least n bytes.
+            """
+        while len(self._read_buffer) < n:
+            self._read_buffer += connection.recv(self.rec_buf_size)
+        
+    def _read(self, connection, n):
+        """Read the next n bytes from the given connection"""
+        self._fill_buffer(connection, n)
+        self._chunk = bytes(self._read_buffer[:n])
+        self._read_buffer = self._read_buffer[n:]
+        return self._chunk
+
+    def read_message(self, connection):
+        """ Reads a single message from the recv buffer, as specified by the format in protocol.py:
+            expects the first 9 bytes to give the length of the remainer of the messsage."""
+        prefix = self._read(connection, PREFIX_SIZE)
+        msg_len = decode_length(prefix)
+        msg = self._read(connection, msg_len)
+        self.log(f"{self._id} RECD: {msg}")
+        return msg
+
+    def send_message(self, socket, message_body):
+        """ Sends a message over the given socket.
+            message_body: a bytes object"""
+        socket.sendall(encode(message_body))
+        
 
 class Server(TCPModule):
 
@@ -42,13 +65,14 @@ class Server(TCPModule):
         """
         super().__init__(port_number=port_number, rec_buf_size=rec_buf_size)
         self._queue_size = queue_size
+        self._id = F"SVR {port_number}"
 
     def _get_response(self):
-        return b'I got this message: ' + self._read
+        return b'I got this message: ' + self._chunk
 
     def _respond(self, connection):
         """Issue a response over the given connection. Depends on server state."""
-        connection.sendall(self._get_response())
+        self.send_message(connection, self._get_response())
 
     def start(self):
         """Starts running the server: OPEN the local port, with no foreign socket specified.
@@ -66,7 +90,7 @@ class Server(TCPModule):
                     connection, address = s.accept()
                     self.log(f"Accepted a connection from {address}")
                     with connection:
-                        self._read_all(connection)
+                        self.read_message(connection)
                         self._respond(connection)
                         
             except KeyboardInterrupt:
@@ -74,6 +98,9 @@ class Server(TCPModule):
 
 
 if __name__ == "__main__":
-    server = Server(56500, rec_buf_size=2, queue_size=1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", default=50000, type=int)
+    args = parser.parse_args()
+    server = Server(args.port, rec_buf_size=1024, queue_size=1)
     logging.basicConfig(level=logging.INFO)
     server.start()
