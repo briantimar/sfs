f24509c827edcdce2654162ff4265e04cd15a050
Brian Timar <timarbrian@gmail.com>
2020-02-26 23:36:56 -0800

    client port numbers

--- tcp_server_v0/client.py
+++ tcp_server_v0/client.py
@@ -10,8 +10,11 @@ class Client(TCPModule):
         super().__init__(port_number=port_number, rec_buf_size=rec_buf_size)
         if self.port_number is not None:
             self.port_number = int(self.port_number)
-            self.log(f"Starting client with static port number {self.port_number}...")
 
+    def set_port_number(self, n):
+        """Updates the current port number of this client."""
+        self.port_number = n
+        self._id = f"CLT {self.port_number}"
 
     def send(self, foreign_addr, foreign_port, message):
         """Send a bytes message to TCP server at address and port specified."""
@@ -22,17 +25,23 @@ class Client(TCPModule):
                 self.log(f"Binding to local port {self.port_number}...")
                 s.bind(('127.0.0.1', self.port_number))
             s.connect((foreign_addr, foreign_port))
-            self.log(f"Using local socket {s.getsockname()}")
 
-            s.sendall(message)
-            s.shutdown(socket.SHUT_RDWR)
+            self.set_port_number(s.getsockname()[1])
+            self.log(f"Using local port {self.port_number}")
+
+            self.send_message(s, message)
+            self.log(f"Message sent.")
+            self.read_message(s)
         self.log("Done.")
         
-    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("msg", default="", type=str)
+    parser.add_argument("-target", default=50000, type=int)
     parser.add_argument("--port", default=None)
     args = parser.parse_args()
+
+    message = bytes(args.msg, 'ascii')
     cl = Client(port_number=args.port)
-    cl.send('127.0.0.1', 56500, b'hello')
+    cl.send('127.0.0.1', args.target, message)
