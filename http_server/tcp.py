02f5781fd4fa18cbaec42fd54c96b636f4d129fa
Brian Timar <timarbrian@gmail.com>
2020-02-28 00:44:33 -0800

    read_all which exhausts socket without timeout

--- http_server/tcp.py
+++ http_server/tcp.py
@@ -1,5 +1,6 @@
 """Module for handling indivdual TCP sockets."""
 import socket
+import select
 
 class TCPModule:
     """One half of a TCP connection."""
@@ -24,17 +25,37 @@ class TCPModule:
 
     def _flush_read(self):
         """Flush the read buffer."""
+        self._chunk = bytes(self._read_buffer)
         self._read_buffer = bytearray(b'')
-
-    def _fill_buffer(self, connection, n):
-        """Fill the read buffer from the given connection, until it contains at least n bytes.
-            """
-        while len(self._read_buffer) < n:
-            self._read_buffer += connection.recv(self.rec_buf_size)
+        return self._chunk
 
     def _read(self, connection, n):
-        """Read the next n bytes from the given connection"""
-        self._fill_buffer(connection, n)
-        self._chunk = bytes(self._read_buffer[:n])
-        self._read_buffer = self._read_buffer[n:]
+        """ Read at most n bytes directly from the connection. Possibly blocking. """
+        try:
+            return connection.recv(n)
+        except socket.timeout:
+            return b''
+        except socket.error:
+            self.log("No data!")
+            return b''
+
+    def read_all(self, connection):
+        """ Read from a connection until the bytestream dries up.
+            Sets the connection to non-blocking mode, and then repeatedly reads from it until a socket.error is raised, 
+            which happens when the buffer is empty.
+            this is a bit fragile, in the sense that part of a message might be delayed, in which case it'll return
+            immediately without seeing the rest. 
+            Does not close the connection.
+
+            Returns: bytes object.
+            
+            """
+        connection.setblocking(0)
+        data = self._read(connection, self.rec_buf_size)
+        while len(data) > 0:
+            self._read_buffer += data
+            data = self._read(connection, self.rec_buf_size)
+        self._flush_read()
         return self._chunk
+        
+
